861a19ad705d09901829630a16a4f751a925c0d1
xxcosita3czxx <kokosove18@gmail.com>
2025-02-10 19:18:29 +0100

    adding into python example

---
+++ bots/discord/langs/python/bot.py
@@ -0,0 +1,10 @@
+import discord
+
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f"Logged in as {client.user}")
+
+client.run("insert token here")
